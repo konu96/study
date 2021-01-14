a4ac3c0e75d9d2e02d3d15237ef0988b89dc4478
konu96 <ukouhei96@gmail.com>
2021-01-14 20:54:22 +0900

    [ADD] SwiftUI のチュートリアル1をやった

---
+++ app/swiftui/Creating-and-Combining-Views/Creating-and-Combining-Views/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Creating-and-Combining-Views
+//
+//  Created by kohei-uno on 2021/01/14.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("20171030025557")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ app/swiftui/Creating-and-Combining-Views/Creating-and-Combining-Views/Creating_and_Combining_ViewsApp.swift
@@ -0,0 +1,18 @@
+//
+//  Creating_and_Combining_ViewsApp.swift
+//  Creating-and-Combining-Views
+//
+//  Created by kohei-uno on 2021/01/14.
+//
+//
+
+import SwiftUI
+
+@main
+struct Creating_and_Combining_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
