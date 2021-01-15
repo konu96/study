776f9e31b67895218f46b3a1b5d806411605234f
konu96 <ukouhei96@gmail.com>
2021-01-15 23:48:24 +0900

    [ADD] creating-a-cardview

---
+++ app/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by kohei-uno on 2021/01/15.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
