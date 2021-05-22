dfe3ce35bf37bc7382c9ba6eeefafcc845c7901e
konu96 <ukouhei96@gmail.com>
2021-05-23 00:28:12 +0900

    dayjs だけを使って日付の比較をする

--- nuxt/date-test/pages/index.vue
+++ nuxt/date-test/pages/index.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="container">
-
+    <ul v-for="dayjs in dayjsList">
+      <li> {{ isToday(dayjs) }}</li>
+    </ul>
+    {{ $dayjs() }}
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import {defineComponent, useContext} from '@nuxtjs/composition-api';
+import { useDate } from '@/compositions/useDate';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
-export default Vue.extend({})
+export default defineComponent({
+  setup() {
+    const { $dayjs } = useContext()
+    $dayjs.extend(utc);
+    $dayjs.extend(timezone);
+    const { isToday } = useDate()
+
+    const dayjsList = [
+      $dayjs('Sat May 21 2021 23:39:32 GMT+0900').format(),
+      $dayjs('Sat May 22 2021 23:39:32 GMT+0900').format(),
+      $dayjs('Sat May 23 2021 23:39:32 GMT+0900').format(),
+    ]
+
+    return {
+      dayjsList,
+      isToday
+    }
+  }
+})
 </script>
 
 <style>
